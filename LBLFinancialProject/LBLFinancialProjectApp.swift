1ef3b727be9287dc11be97fec8d87155c1ee6522
Jake Sims <setsailswift@gmail.com>
2023-12-01 09:20:29 -0500

    Initial Commit

---
+++ LBLFinancialProject/LBLFinancialProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBLFinancialProjectApp.swift
+//  LBLFinancialProject
+//
+//  Created by Jake Sims on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct LBLFinancialProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
