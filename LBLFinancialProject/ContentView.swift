11d9eb200a1661824511a07faca2d97865d01f53
Jake Sims <setsailswift@gmail.com>
2023-12-03 14:19:45 -0500

    Update ContentView to use ScrollView

--- LBLFinancialProject/ContentView.swift
+++ LBLFinancialProject/ContentView.swift
@@ -11,9 +11,28 @@ struct ContentView: View {
     @StateObject private var commentViewModel = CommentsViewModel()
     
     var body: some View {
-        List(commentViewModel.comments) { comment in 
-            Cell(comment: comment)
+        // Placed into a NavigationStack for the intention that as the app
+        // grows, we'd more than likely navigation to another view when a cell
+        // is selected. Also to enable the navigation title.
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ForEach(commentViewModel.comments) {
+                        Cell(comment: $0)
+                        Divider()
+                    }
+                }
+            }
+            .navigationTitle("Comments")
+            .padding(.horizontal)
         }
+        
+        // Disabled using list temporarily as pressing the entire cell
+        // would invoke the PhotosPicker, when it should just occur when
+        // the image is tapped.
+//        List(commentViewModel.comments) { comment in
+//            Cell(comment: comment)
+//        }
     }
 }
 
